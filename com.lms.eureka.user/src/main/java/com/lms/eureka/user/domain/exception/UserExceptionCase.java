d77628cc4381f46ccbd551ecebb002decd54d620
hosu <76641246+junghojune@users.noreply.github.com>
2024-09-10 16:40:20 +0900

    feat: 사용자 조회 API 개발(feign)

--- com.lms.eureka.user/src/main/java/com/lms/eureka/user/domain/exception/UserExceptionCase.java
+++ com.lms.eureka.user/src/main/java/com/lms/eureka/user/domain/exception/UserExceptionCase.java
@@ -8,12 +8,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserExceptionCase {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당하는 유저가 존재하지 않습니다."),
-    PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED.value(), "해당하는 유저가 존재하지 않습니다.")
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 유저가 존재하지 않습니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "해당하는 유저가 존재하지 않습니다.")
 
     ;
 
-    private final int httpStatus;
+    private final HttpStatus httpStatus;
     private final String message;
 
 }
---
+++ com.lms.eureka.user/src/main/java/com/lms/eureka/user/presentation/controller/FeignApiController.java
@@ -0,0 +1,31 @@
+package com.lms.eureka.user.presentation.controller;
+
+import com.lms.eureka.user.application.service.UserService;
+import com.lms.eureka.user.domain.exception.UserException;
+import com.lms.eureka.user.domain.exception.UserExceptionCase;
+import com.lms.eureka.user.presentation.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class FeignApiController {
+
+    private final UserService userService;
+
+    @GetMapping("/{username}")
+    public CommonResponse<Void> getUser(@PathVariable("username") String username) {
+       boolean result = userService.getUserInfoByUserName(username) != null;
+
+       if(!result) {
+           throw new UserException(UserExceptionCase.USER_NOT_FOUND);
+       }
+       else {
+           return CommonResponse.success(null);
+       }
+    }
+}
