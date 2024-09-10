88b5bfea87090efaae87dbe890b53e527e2ce412
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-09-10 20:31:57 +0900

    fix responses and queryDsl of Company domain, add delete-product

--- com.lms.eureka.company/src/main/java/com/lms/eureka/company/presentation/request/CompanyCreateRequest.java
+++ com.lms.eureka.company/src/main/java/com/lms/eureka/company/presentation/request/CompanyCreateRequest.java
@@ -2,5 +2,5 @@ package com.lms.eureka.company.presentation.request;
 
 import java.util.UUID;
 
-public record CompanyCreateRequest(String name, String address, UUID hubId) {
+public record CompanyCreateRequest(String name, String address, String companyType, UUID hubId) {
 }
